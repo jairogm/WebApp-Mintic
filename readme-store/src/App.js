b817f7e0858cdebb405c3531160edd4ca1a992f7
Crihstian Molina <cristiancm1235@gmail.com>
2021-10-05 22:01:03 -0500

    Stable 1.0

---
+++ readme-store/src/App.js
@@ -0,0 +1,19 @@
+import HeaderComponent from './components/HeaderComponent'
+import ContentComponent from './components/ContentComponent';
+import FooterComponent from './components/FooterComponent';
+
+import './components/HeaderComponent.css'
+import './components/ContentComponent.css'
+import './components/FooterComponent.css'
+
+function App() {
+  return (
+    <>
+      <HeaderComponent />
+      <ContentComponent />
+      <FooterComponent />
+    </>
+  );
+}
+
+export default App;
