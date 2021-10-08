dc1bb77ea499fe6f35c7aeaf1e231887e1d82b53
LauMontana <lauracamila2417@gmail.com>
2021-10-07 23:17:32 -0500

    Paso codigo del login como react component

--- readme-store/src/App.js
+++ readme-store/src/App.js
@@ -1,6 +1,14 @@
+import React from "react";
+import './App.css';
+//import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+
+
 import HeaderComponent from './components/HeaderComponent'
 import ContentComponent from './components/ContentComponent';
 import FooterComponent from './components/FooterComponent';
+import login from './components/login'
+
 
 import './components/css/HeaderComponent.css'
 import './components/css/ContentComponent.css'
@@ -9,10 +17,21 @@ import './components/css/FooterComponent.css'
 function App() {
   return (
     <>
-      <HeaderComponent />
-      <ContentComponent />
-      <FooterComponent />
+    <HeaderComponent />
+    <ContentComponent />
+    <FooterComponent />
     </>
+    //<Router>
+      //<HeaderComponent />
+      //<div className="container p-4">
+     // <Route path="/login" exact component={login} />
+      //</div>
+      //<ContentComponent />
+     // <FooterComponent />
+      
+
+    //</Router>
+
   );
 }
 
