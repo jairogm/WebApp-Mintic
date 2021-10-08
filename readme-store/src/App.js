2bf8f4ab5a08fdf82e16dc55b3b5ab48c10a3e46
Jairo Garces Montes <jairogarces1@gmail.com>
2021-10-08 18:27:32 -0500

    Add Router to main page

--- readme-store/src/App.js
+++ readme-store/src/App.js
@@ -1,17 +1,55 @@
-import HeaderComponent from './components/HeaderComponent'
-import ContentComponent from './components/ContentComponent';
-import FooterComponent from './components/FooterComponent';
+import HeaderComponent from "./components/HeaderComponent";
+import ContentComponent from "./components/ContentComponent";
+import FooterComponent from "./components/FooterComponent";
+import GestionUsuarios from "./components/Gestion_Usuarios";
 
-import './components/css/HeaderComponent.css'
-import './components/css/ContentComponent.css'
-import './components/css/FooterComponent.css'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import "./components/css/HeaderComponent.css";
+import "./components/css/ContentComponent.css";
+import "./components/css/FooterComponent.css";
 
 function App() {
   return (
     <>
-      <HeaderComponent />
-      <ContentComponent/>
-      <FooterComponent />
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/gestionusuarios">About</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+          <Switch>
+            <Route exact path="/">
+              <HeaderComponent />
+              <ContentComponent />
+              <FooterComponent />
+            </Route>
+            <Route path="/gestionusuarios">
+              <GestionUsuarios />
+            </Route>
+            <Route path="/dashboard">
+              <h1>Hello</h1>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </>
   );
 }
--- readme-store/src/components/FooterComponent.js
+++ readme-store/src/components/FooterComponent.js
@@ -2,8 +2,8 @@
 const FooterComponent = () => {
     return (<footer>
         <div class="footer-container">
-            <a href="#">Politica de Privacidad</a> |
-            <a href="#">Terminos & condiciones</a> | &copy Copyright 2021- ReadMe Libreria - Todos los derechos revervados.
+            <a href="!#">Politica de Privacidad</a> |
+            <a href="!#">Terminos & condiciones</a> | &copy Copyright 2021- ReadMe Libreria - Todos los derechos revervados.
         </div>
     </footer>);
 }
