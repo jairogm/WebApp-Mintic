a71efc96507571e1d1a8845e9518ef327e203b05
Juan Pino <juanrespolo@gmail.com>
2021-10-09 10:50:48 -0500

    GestionUsuarios half funcionality

--- readme-store/src/components/Gestion_Usuarios.js
+++ readme-store/src/components/Gestion_Usuarios.js
@@ -1,66 +1,102 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 import './css/Gestion_Usuarios.css'
 
+
+class UsuarioTabla extends Component {
+    render() {
+        return (
+            <>
+                    <div className={this.props.row}>{this.props.user.id}</div>
+                    <div className={this.props.row}>{this.props.user.name}</div>
+                    <div className={this.props.row}>{this.props.user.username}</div>
+                    <div className={this.props.row}>{this.props.user.rol}</div>
+                    <div className={this.props.row}>{this.props.user.status}</div>
+               
+            </>)
+    }
+}
+
+
+
+
+
 export default class GestionUsusarios extends Component {
+
+    async componentDidMount(){
+        const res =  await axios.get("https://readme-store-api.herokuapp.com/api/products")
+        console.log(res)
+        const users = res.data.map(user =>{
+            return {
+                id : user._id,
+                name: user.sku,
+                username: user.price,
+                rol: user.author,
+                status: user.title
+            }
+        })
+
+        this.setState({users:users}) 
+    }
+
+    state = {
+        users: [],
+        idSearch : "",
+        nameSearch: "",
+        userSearch :""
+    }
+
+
+    printUsers() {
+        let row = "row-pair";
+        return <>
+        {this.state.users.map((user) => {
+            row = row === "row-odd" ? "row-pair" : "row-odd"
+            return <UsuarioTabla key ={user.id} user = {user} row = {row}/>
+        })}
+        </>
+    }
+
+    onChange = (e) =>{
+        this.setState({[e.target.name]: e.target.value})
+    }
+
     render() {
         return (
             <div>
-                <section class="section-1-container">
-                    <div class="search-box">
-                        <div class="search-row">
-                            <p class="p-search">Buscar ID:</p>
-                            <input type="text" class="search-input" placeholder="Buscar" />
+                <section className="gest-usu-section-1-container">
+                    <div className="gest-usu-search-box">
+                        <div className="gest-usu-search-row">
+                            <p className="gest-usu-p-search">Buscar ID:</p>
+                            <input type="text" name="idSearch" className="gest-usu-search-input" placeholder="Buscar" value={this.state.idSearch} onChange={this.onChange}/>
                         </div>
-                        <div class="search-row">
-                            <p class="p-search">Nombre:</p>
-                            <input type="text" class="search-input" placeholder="Nombre" />
+                        <div className="gest-usu-search-row">
+                            <p className="gest-usu-p-search">Nombre:</p>
+                            <input type="text" name="nameSearch" className="gest-usu-search-input" placeholder="Nombre" value={this.state.nameSearch} onChange={this.onChange}/>
                         </div>
-                        <div class="search-row">
-                            <p class="p-search">Usuario:</p>
-                            <input type="text" class="search-input" placeholder="Usuario" />
+                        <div className="gest-usu-search-row">
+                            <p className="gest-usu-p-search">Usuario:</p>
+                            <input type="text" name="userSearch"className="gest-usu-search-input" placeholder="Usuario" value={this.state.userSearch} onChange={this.onChange}/>
                         </div>
 
 
-                        <div class="search-box-buttons">
-                            <button class="btn btn-search">Buscar</button>
-                            <button class="btn btn-search">Actualizar</button>
+                        <div className="gest-usu-search-box-buttons">
+                            <button className="gest-usu-btn gest-usu-btn-search">Buscar</button>
+                            <button className="gest-usu-btn gest-usu-btn-search">Actualizar</button>
                         </div>
                     </div>
 
-                    <div class="user-table">
-                        <div class="row-head">Id Usuario</div>
-                        <div class="row-head">Nombre</div>
-                        <div class="row-head">Usuario</div>
-                        <div class="row-head">Rol</div>
-                        <div class="row-head">Estado</div>
+                    <div className="gest-usu-user-table">
+                        <div className="gest-usu-row-head">Id Usuario</div>
+                        <div className="gest-usu-row-head">Nombre</div>
+                        <div className="gest-usu-row-head">Usuario</div>
+                        <div className="gest-usu-row-head">Rol</div>
+                        <div className="gest-usu-row-head">Estado</div>
 
-                        
-                        <div class="row-odd">Id Usuario</div>
-                        <div class="row-odd">Nombre</div>
-                        <div class="row-odd">Usuario</div>
-                        <div class="row-odd">Rol</div>
-                        <div class="row-odd">Estado</div>
-
-                        
-                        <div class="row-pair">Id Usuario</div>
-                        <div class="row-pair">Nombre</div>
-                        <div class="row-pair">Usuario</div>
-                        <div class="row-pair">Rol</div>
-                        <div class="row-pair">Estado</div>
-
-                        
-                        <div class="row-odd">Id Usuario</div>
-                        <div class="row-odd">Nombre</div>
-                        <div class="row-odd">Usuario</div>
-                        <div class="row-odd">Rol</div>
-                        <div class="row-odd">Estado</div>
-
-                        
-                        <div class="row-pair">Id Usuario</div>
-                        <div class="row-pair">Nombre</div>
-                        <div class="row-pair">Usuario</div>
-                        <div class="row-pair">Rol</div>
-                        <div class="row-pair">Estado</div>
+                        {
+                            this.printUsers()
+                        }
+                
                     </div>
 
 
