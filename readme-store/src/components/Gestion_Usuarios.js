1a1c795aa2c5d32337e0b3f07219b4606ac255c9
Jairo Garces Montes <jairogarces1@gmail.com>
2021-10-09 11:16:12 -0500

    Add ejemplo popup

--- readme-store/src/components/Gestion_Usuarios.js
+++ readme-store/src/components/Gestion_Usuarios.js
@@ -1,71 +1,72 @@
-import React, { Component } from 'react'
-import './css/Gestion_Usuarios.css'
+import React, { useState } from "react";
+import "./css/Gestion_Usuarios.css";
 
-export default class GestionUsusarios extends Component {
-    render() {
-        return (
-            <div>
-                <section class="section-1-container">
-                    <div class="search-box">
-                        <div class="search-row">
-                            <p class="p-search">Buscar ID:</p>
-                            <input type="text" class="search-input" placeholder="Buscar" />
-                        </div>
-                        <div class="search-row">
-                            <p class="p-search">Nombre:</p>
-                            <input type="text" class="search-input" placeholder="Nombre" />
-                        </div>
-                        <div class="search-row">
-                            <p class="p-search">Usuario:</p>
-                            <input type="text" class="search-input" placeholder="Usuario" />
-                        </div>
+import PopUp from "./PopUp";
+import usePopUp from "../hooks/usePopUp";
+function Gestion_Usuarios() {
+  const [isOpenModal, openModal, closeModal] = usePopUp();
 
+  return (
+    <div>
+        
+      <section class="section-1-container">
+        <div class="search-box">
+          <div class="search-row">
+            <p class="p-search">Buscar ID:</p>
+            <input type="text" class="search-input" placeholder="Buscar" />
+          </div>
+          <div class="search-row">
+            <p class="p-search">Nombre:</p>
+            <input type="text" class="search-input" placeholder="Nombre" />
+          </div>
+          <div class="search-row">
+            <p class="p-search">Usuario:</p>
+            <input type="text" class="search-input" placeholder="Usuario" />
+          </div>
 
-                        <div class="search-box-buttons">
-                            <button class="btn btn-search">Buscar</button>
-                            <button class="btn btn-search">Actualizar</button>
-                        </div>
-                    </div>
+          <div class="search-box-buttons">
+            <button class="btn btn-search">Buscar</button>
+            <button class="btn btn-search">Actualizar</button>
+            <button onClick={openModal} className="btn btn-search">POPUP</button>
+            <PopUp closeModal={closeModal} isOpen={isOpenModal}/>
 
-                    <div class="user-table">
-                        <div class="row-head">Id Usuario</div>
-                        <div class="row-head">Nombre</div>
-                        <div class="row-head">Usuario</div>
-                        <div class="row-head">Rol</div>
-                        <div class="row-head">Estado</div>
+          </div>
+        </div>
 
-                        
-                        <div class="row-odd">Id Usuario</div>
-                        <div class="row-odd">Nombre</div>
-                        <div class="row-odd">Usuario</div>
-                        <div class="row-odd">Rol</div>
-                        <div class="row-odd">Estado</div>
+        <div class="user-table">
+          <div class="row-head">Id Usuario</div>
+          <div class="row-head">Nombre</div>
+          <div class="row-head">Usuario</div>
+          <div class="row-head">Rol</div>
+          <div class="row-head">Estado</div>
 
-                        
-                        <div class="row-pair">Id Usuario</div>
-                        <div class="row-pair">Nombre</div>
-                        <div class="row-pair">Usuario</div>
-                        <div class="row-pair">Rol</div>
-                        <div class="row-pair">Estado</div>
+          <div class="row-odd">Id Usuario</div>
+          <div class="row-odd">Nombre</div>
+          <div class="row-odd">Usuario</div>
+          <div class="row-odd">Rol</div>
+          <div class="row-odd">Estado</div>
 
-                        
-                        <div class="row-odd">Id Usuario</div>
-                        <div class="row-odd">Nombre</div>
-                        <div class="row-odd">Usuario</div>
-                        <div class="row-odd">Rol</div>
-                        <div class="row-odd">Estado</div>
+          <div class="row-pair">Id Usuario</div>
+          <div class="row-pair">Nombre</div>
+          <div class="row-pair">Usuario</div>
+          <div class="row-pair">Rol</div>
+          <div class="row-pair">Estado</div>
 
-                        
-                        <div class="row-pair">Id Usuario</div>
-                        <div class="row-pair">Nombre</div>
-                        <div class="row-pair">Usuario</div>
-                        <div class="row-pair">Rol</div>
-                        <div class="row-pair">Estado</div>
-                    </div>
+          <div class="row-odd">Id Usuario</div>
+          <div class="row-odd">Nombre</div>
+          <div class="row-odd">Usuario</div>
+          <div class="row-odd">Rol</div>
+          <div class="row-odd">Estado</div>
 
-
-                </section>
-            </div>
-        )
-    }
+          <div class="row-pair">Id Usuario</div>
+          <div class="row-pair">Nombre</div>
+          <div class="row-pair">Usuario</div>
+          <div class="row-pair">Rol</div>
+          <div class="row-pair">Estado</div>
+        </div>
+      </section>
+    </div>
+  );
 }
+
+export default Gestion_Usuarios;
