4c0eec85043024e5be971f5b92fea728bb18a34c
Jairo Garces Montes <jairogarces1@gmail.com>
2021-10-14 21:47:00 -0500

    Fix errors

--- readme-store/src/components/login-form/Login.js
+++ readme-store/src/components/login-form/Login.js
@@ -41,7 +41,7 @@ function Login() {
     };
 
     return (
-        <div>
+        <div className="body">
             <div className="login-box">
                 <img src={LogoM} alt="Logo" href='!#' />
                 <h1> Bienvenido!</h1>
