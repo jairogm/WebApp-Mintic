fc1cc1269b18b1e27dfcce6915b65edae6c90803
Juan Pino <juanrespolo@gmail.com>
2021-10-16 10:49:55 -0500

    buscarVenta css corrections

--- readme-store/src/components/modulovendedores/buscarVenta/BuscarVenta.js
+++ readme-store/src/components/modulovendedores/buscarVenta/BuscarVenta.js
@@ -1,25 +1,26 @@
+import "../../css/buscar-venta.css"
 
 const BuscarVenta = () => {
     return (<>
-        <section class="form">
-            <div class="form-title">
+        <section className="busc-venta-form">
+            <div className="busc-venta-form-title">
                 <span>Buscar Ventas</span>
             </div>
             <form>
-                <div class="input">
+                <div className="busc-venta-input">
                     <label for="">ID Venta</label><input type="text" placeholder="ID Venta" name="" id="" />
                 </div>
-                <div class="input">
+                <div className="busc-venta-input">
                     <label for="">Identificacion del Cliente</label><input type="text" placeholder="Documento Identidad" name="" id="" />
                 </div>
-                <div class="input">
+                <div className="busc-venta-input">
                     <label for="">Nombre del Cliente</label><input type="text" placeholder="Nombre del Cliente" name="" id="" />
                 </div>
-                <div class="input">
+                <div className="busc-venta-input">
                     <label for="">Nombre Vendedor</label><input type="text" placeholder="Nombre Vendedor" name="" id="" />
                 </div>
             </form>
-            <div class="form-button">
+            <div className="busc-venta-form-button">
                 <input type="button" value="Buscar Venta" />
             </div>
         </section>
