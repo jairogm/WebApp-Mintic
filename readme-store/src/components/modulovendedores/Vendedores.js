8b56c36906be1496905a6a3edf69573e57ac5f45
LauMontana <lauracamila2417@gmail.com>
2021-10-16 18:47:37 -0500

    Agrego codigo cambio entre componentes m.vendedor

--- readme-store/src/components/modulovendedores/Vendedores.js
+++ readme-store/src/components/modulovendedores/Vendedores.js
@@ -1,25 +1,51 @@
-import BuscarVenta from "./buscarVenta/BuscarVenta";
-import HeaderSimple from "../headerSimple/HeaderSimple";
+import React, { Component, useState } from 'react';
 import FooterComponent from "../footer/FooterComponent";
+import BuscarVenta from "./buscarVenta/BuscarVenta";
+import RegistrarVenta from "./registrarVenta/RegistrarVenta";
+import HeaderSimple from "../headerSimple/HeaderSimple";
+
+
 
 import "./../css/vendedores.css"
 
-const Vendedores = () => {
-    return (<>
-        <HeaderSimple/>
-        <section class="container">
-            <section class="side-menu">
-                <a href="!#">Información Vendedor</a>
-                <a href="!#">Registrar Venta</a>
-                <a href="!#">Ventas del Mes</a>
-                <a href="!#">Buscar Ventas</a>
+function Vendedores() {
+    const[openRegistrarVenta, SetOpenRegistrarVenta] = useState(false);
+    const [openBuscarVenta, SetOpenBuscarVenta] = useState(false);
+
+    const printForm = ()=> {
+        if (openRegistrarVenta) {
+            return <RegistrarVenta/>
+        }else if(openBuscarVenta){
+            return <BuscarVenta/>
+        }
+    }
+
+    const setStates = (RegistrarVenta,buscarVenta) => {
+        SetOpenRegistrarVenta(RegistrarVenta)
+        SetOpenBuscarVenta(buscarVenta)  
+    }
+
+
+    return (
+        <div>
+            <HeaderSimple/>
+            <section className="container">
+                <section className="side-menu">
+                    <button >Informacion del vendedor</button>
+                    <button onClick={() =>{setStates(true,false,false,false)}}>Registrar Venta</button>
+                    <button >Ventas del Mes</button>
+                    <button onClick={() =>{setStates(false,true,false,false)}}>Buscar Ventas</button>
+                </section>
+
+                <section className="form">
+                    {
+                        printForm()
+                    }
+                </section>
             </section>
-            <BuscarVenta/>
-        </section>
-        <FooterComponent/>
-    </>)
+            <FooterComponent />
+        </div>
+    )
 }
-
-
 
 export default Vendedores;
