a69add56c645e37c954139c666d66e872c805310
LauMontana <lauracamila2417@gmail.com>
2021-10-20 21:51:51 -0500

    Agrego datagrid table de registro ventas sin funcion m.vendedor

--- readme-store/src/components/modulovendedores/RegistroVentas.js
+++ readme-store/src/components/modulovendedores/RegistroVentas.js
@@ -1,133 +1,32 @@
-import React, { useState, useEffect, Component, setTimeout } from "react";
-import GestionVentaspopup from '../moduloadmin/gestionventas/GestionVentasPopup';
-import "../css/GestionVentas.css"
-import usePopUp from "../../hooks/usePopUp";
-import axios from 'axios';
+import React from "react";
+import { DataGrid } from "@mui/x-data-grid";
+
+function RegistroVentas() {
+    const columns = [
+        { field: 'id', headerName: 'ID Venta', width: 150 },
+        { field: 'name', headerName: 'Nombre Cliente', width: 230 },
+        { field: 'total', headerName: 'Total venta', type: 'number', width: 150},
+        { field: 'date', headerName: 'Fecha', type: 'date', width: 150}
+       
+      ];
+      
+      const rows = [
+        { id: 1, name: 'Jon Snow', total: 35000, date: 20102021 },
+        { id: 2, name: '', total: '', date: '20102021' }
+      ];
+      
 
 
-
-function RegistroVentas({ setStates, setVentaEstado, renderAgain, SetRenderAgain }) {
-    const [isOpenModal, openModal, closeModal] = usePopUp();
-    const [sales, setSales] = useState([])
-    const [idSearch, setIdSearch] = useState("")
-    const [IdClSearch, setIdClSearch] = useState("")
-    const [NameClSearch, setNameClSearch] = useState("")
-    const [DateSearch, setDateSearch] = useState("")
-    const [WasFound, setWasFound] = useState(true)
-    const [saleData, setSaleData] = useState({})
-
-    const getSales = async () => {
-        const res = await axios.get("https://readme-store-api.herokuapp.com/api/sales")
-        const salesGetted = res.data.map(sale => {
-            return {
-                id: sale._id,
-                clientid: sale.clientid,
-                clientname: sale.clientname,
-                date: sale.date
-            }
-        });
-        setSales(salesGetted)
-    }
-
-    useEffect(() => {
-        getSales();
-        if (renderAgain) {
-            SetRenderAgain(false);
-            setStates(false,false,true,false,false)
-        }
-    }, []);
-
-    const close = (isModidy) => {
-        closeModal();
-        if (isModidy) {
-            setVentaEstado(saleData)
-            setStates(false,false,false,false,true)
-        } else {
-            setWasFound(true)
-        }
-    }
-
-    const printSales = () => {
-        let row = "row-pair";
-        return <>
-            {sales.map((sale) => {
-                row = row === "row-odd" ? "row-pair" : "row-odd"
-                return <RegistroVentas key={sale.id} sale={sale} row={row} />
-            })}
-        </>
-    }
-
-    const notParams = () => {
-        if (idSearch === "" && NameClSearch === "" && IdClSearch === "" && DateSearch === "") {
-            return true;
-        }
-        return false;
-    }
-
-    const Search = async () => {
-        let res = ""
-        if (notParams()) {
-
-            setWasFound(false);
-
-        } else {
-
-            res = await axios.get("https://readme-store-api.herokuapp.com/api/sales?id=" + idSearch + "&clientid=" + IdClSearch + "&clientname=" + NameClSearch + "&date=" + DateSearch)
-
-            if (res.data.length === 0) {
-                setWasFound(false)
-            } else {
-                setSaleData(res.data[0])
-            }
-
-        }
-        openModal()
-    }
-
-    return (
-        <div>
-
-            <section className="gest-ven-section-1-container">
-                <div className="gest-ven-search-box">
-                    <div className="gest-ven-search-row">
-                        <p className="gest-ven-p-search">Buscar ID:</p>
-                        <input type="text" className="gest-ven-search-input" placeholder="ID" onChange={(e) => setIdSearch(e.target.value)} value={idSearch} />
-                    </div>
-                    <div className="gest-ven-search-row">
-                        <p className="gest-ven-p-search">ID Cliente:</p>
-                        <input type="text" className="gest-ven-search-input" placeholder="ID Cliente" onChange={(e) => setIdClSearch(e.target.value)} value={IdClSearch} />
-                    </div>
-                    <div className="gest-ven-search-row">
-                        <p className="gest-ven-p-search">Nombre Cliente:</p>
-                        <input type="text" className="gest-ven-search-input" placeholder="Nombre Cliente" onChange={(e) => setNameClSearch(e.target.value)} value={NameClSearch} />
-                    </div>
-                    <div className="gest-ven-search-row">
-                        <p className="gest-ven-p-search">Fecha:</p>
-                        <input type="text" className="gest-ven-search-input" placeholder="Fecha Venta" onChange={(e) => setDateSearch(e.target.value)} value={DateSearch} />
-                    </div>
-
-                    <div className="gest-ven-search-box-buttons">
-                        <button onClick={() => { Search() }} className="gest-ven-btn gest-ven-btn-search">Buscar</button>
-                        <GestionVentaspopup isOpen={isOpenModal} WasFound={WasFound} close={close} data={saleData} />
-
-                    </div>
-                </div>
-
-                <div className="gest-ven-user-table">
-                    <div className="gest-ven-row-head">Id Venta</div>
-                    <div className="gest-ven-row-head">Id Cliente</div>
-                    <div className="gest-ven-row-head">Nombre Cliente</div>
-                    <div className="gest-ven-row-head">Fecha</div>
-
-
-                    {
-                        printSales()
-                    }
-                </div>
-            </section>
-        </div>
-    )
+  return (
+    <div style={{ height: 400, width: "100%" }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={4}
+        rowsPerPageOptions={[5]}
+        />
+    </div>
+  );
 }
-
 
 export default RegistroVentas;
--- readme-store/src/components/modulovendedores/Vendedores.js
+++ readme-store/src/components/modulovendedores/Vendedores.js
@@ -10,15 +10,13 @@ import RegistroVentas from './RegistroVentas';
 import "./../css/vendedores.css"
 
 function Vendedores() {
-    const[openGestionVendedores, SetopenGestionVendedores] = useState(false);
+    
     const[openRegistrarVenta, SetOpenRegistrarVenta] = useState(false);
     const[openRegistroVentas, SetopenRegistroVentas] = useState(false);
     const[openBuscarVenta, SetOpenBuscarVenta] = useState(false);
 
     const printForm = () => {
-        if (openGestionVendedores) {
-            return <GestionVendedores/>
-        }else if(openRegistrarVenta){
+        if(openRegistrarVenta){
             return <RegistrarVenta/>
         }else if(openRegistroVentas){
             return <RegistroVentas/>  
@@ -27,8 +25,7 @@ function Vendedores() {
         }
     }
 
-    const setStates = (GestionVendedores,RegistrarVenta,RegistroVentas,buscarVenta) => {
-        SetopenGestionVendedores(GestionVendedores)
+    const setStates = (RegistrarVenta,RegistroVentas,buscarVenta) => {
         SetOpenRegistrarVenta(RegistrarVenta)
         SetopenRegistroVentas(RegistroVentas)
         SetOpenBuscarVenta(buscarVenta)  
@@ -40,10 +37,9 @@ function Vendedores() {
             <HeaderSimple/>
             <section className="container">
                 <section className="side-menu">
-                    <button onClick={()=>{setStates(true,false,false,false)}}>Informacion del vendedor</button>
-                    <button onClick={() =>{setStates(false,true,false,false)}}>Registrar Venta</button>
-                    <button onClick={() =>{setStates(false,false,true,false)}}>Ventas del Mes</button>
-                    <button onClick={() =>{setStates(false,false,false,true)}}>Buscar Ventas</button>
+                    <button onClick={() =>{setStates(true,false,false)}}>Registrar Venta</button>
+                    <button onClick={() =>{setStates(false,true,false)}}>Ventas del Mes</button>
+                    <button onClick={() =>{setStates(false,false,true)}}>Buscar Ventas</button>
                 </section>
 
                 <section className="form">
